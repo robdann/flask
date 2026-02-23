c135ab84b880057645247a5e7fec0fe809efac04
Robert Dann <robdann@gmail.com>
2026-02-23 08:40:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI
+from piper import PiperVoice
+import hashlib
 import os
 
-app = Flask(__name__)
+app = FastAPI()
 
+voice = PiperVoice.load("en_GB-alan-medium.onnx")
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.post("/speak")
+def speak(text: str):
+    h = hashlib.sha1(text.encode()).hexdigest()
+    path = f"cache/{h}.wav"
 
+    if not os.path.exists(path):
+        voice.synthesize(text, path)
+
+    return {"url": f"/audio/{h}.wav"}
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
